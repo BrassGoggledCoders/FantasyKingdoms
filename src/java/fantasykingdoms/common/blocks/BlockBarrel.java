cff957c850c6d99f6d88bfaa0ec7879d2d4f35ec
Arona Jones <warlordjones@gmail.com>
2015-02-14 16:03:21 +0000

    Barrel textures

--- src/java/fantasykingdoms/common/blocks/BlockBarrel.java
+++ src/java/fantasykingdoms/common/blocks/BlockBarrel.java
@@ -1,13 +1,20 @@
 package fantasykingdoms.common.blocks;
 
 import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.IIcon;
 import net.minecraft.world.World;
+
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 
 import fantasykingdoms.common.tiles.TileBarrel;
 
 public class BlockBarrel extends BaseContainerBlock
 {
+	@SideOnly(Side.CLIENT)
+	private IIcon iconTop, iconBottom;
 
 	public BlockBarrel(Material mat)
 	{
@@ -18,6 +25,32 @@ public class BlockBarrel extends BaseContainerBlock
 	public TileEntity createNewTileEntity(World p_149915_1_, int p_149915_2_)
 	{
 		return new TileBarrel();
+	}
+
+	@SideOnly(Side.CLIENT)
+	@Override
+	public IIcon getIcon(int side, int meta)
+	{
+		switch (side)
+		{
+		case 0:
+			return this.iconBottom; // bottom
+
+		case 1:
+			return this.iconTop; // top
+
+		default:
+			return this.blockIcon; // sides
+		}
+	}
+
+	@SideOnly(Side.CLIENT)
+	@Override
+	public void registerBlockIcons(IIconRegister icon)
+	{
+		this.blockIcon = icon.registerIcon("fantasykingdoms:blockBarrelSide");
+		this.iconTop = icon.registerIcon("fantasykingdoms:blockBarrelEndFull");
+		this.iconBottom = icon.registerIcon("fantasykingdoms:blockEndBarrelClosed");
 	}
 
 }
