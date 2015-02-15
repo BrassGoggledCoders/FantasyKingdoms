ba32308f5b6763ae6f98e1c68fda8a94cf036779
Toby <tobyreed0@gmail.com>
2015-02-15 12:13:16 +0000

    Optimised Imports

--- src/java/fantasykingdoms/common/blocks/BaseBlock.java
+++ src/java/fantasykingdoms/common/blocks/BaseBlock.java
@@ -12,14 +12,13 @@
  */
 package fantasykingdoms.common.blocks;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.util.IIcon;
-
-import cpw.mods.fml.relauncher.Side;
-import cpw.mods.fml.relauncher.SideOnly;
 
 /**
  * @author Surseance
