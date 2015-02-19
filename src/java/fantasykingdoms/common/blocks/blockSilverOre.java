2f6528796a7d9d65d0c83fc215ba1ff43dc86517
Toby <tobyreed0@gmail.com>
2015-02-19 19:06:56 +0000

    Classed more blocks and made changes to blockRegistry.

---
+++ src/java/fantasykingdoms/common/blocks/blockSilverOre.java
@@ -0,0 +1,12 @@
+package fantasykingdoms.common.blocks;
+/**
+ * Created by Toby on 19/02/2015.
+ */
+public class blockSilverOre extends BlockOreFK
+{
+    public blockSilverOre()
+    {
+        super();
+
+    }
+}
---
+++ src/java/fantasykingdoms/common/blocks/blockTinOre.java
@@ -0,0 +1,14 @@
+package fantasykingdoms.common.blocks;
+/**
+ * Created by Toby on 19/02/2015.
+ */
+public class blockTinOre extends BlockOreFK
+{
+    public blockTinOre()
+    {
+        super();
+        this.setBlockName("TinOre");
+        this.setHarvestLevel("pickaxe", 2);
+    }
+
+}
