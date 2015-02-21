3c027badb35daedb03168fde7c16493f3c8ced39
Arona Jones <warlordjones@gmail.com>
2015-02-21 15:03:56 +0000

    Fix up items

--- src/java/fantasykingdoms/common/init/InitItems.java
+++ src/java/fantasykingdoms/common/init/InitItems.java
@@ -1,33 +1,37 @@
 package fantasykingdoms.common.init;
 
+import net.minecraft.item.Item;
+
 import cpw.mods.fml.common.registry.GameRegistry;
 
 import fantasykingdoms.common.items.BaseItem;
 import fantasykingdoms.common.items.ItemEmptyTankard;
 import fantasykingdoms.common.items.ItemTankard;
-import fantasykingdoms.common.items.itemAmethyst;
-import fantasykingdoms.common.items.itemRuby;
-import fantasykingdoms.common.items.itemSapphire;
 
 /**
  * Created by Toby on 20/02/2015.
  */
 public class InitItems
 {
-	public static final BaseItem itemRuby = new itemRuby();
-	public static final BaseItem itemSapphire = new itemSapphire();
-	public static final BaseItem itemAmethyst = new itemAmethyst();
+	public static Item itemRuby;
+	public static Item itemSapphire;
+	public static Item itemAmethyst;
 
-	public static final BaseItem itemEmptyTankard = new ItemEmptyTankard();
-	public static final BaseItem itemFullTankard = new ItemTankard();
+	public static Item itemEmptyTankard;
+	public static Item itemFullTankard;
 
 	public static void registerItems()
 	{
+		itemRuby = new BaseItem().setUnlocalizedName("itemRuby");
 		GameRegistry.registerItem(itemRuby, "itemRuby");
+		itemSapphire = new BaseItem().setUnlocalizedName("itemSapphire");
 		GameRegistry.registerItem(itemSapphire, "itemSapphire");
+		itemAmethyst = new BaseItem().setUnlocalizedName("itemAmethyst");
 		GameRegistry.registerItem(itemAmethyst, "itemAmethyst");
 
+		itemEmptyTankard = new ItemEmptyTankard().setUnlocalizedName("itemEmptyTankard");
 		GameRegistry.registerItem(itemEmptyTankard, "ItemEmptyTankard");
+		itemFullTankard = new ItemTankard().setUnlocalizedName("itemFullTankard");
 		GameRegistry.registerItem(itemFullTankard, "ItemTankard");
 	}
 }
