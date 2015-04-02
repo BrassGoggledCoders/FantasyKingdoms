db25f1043f43adb26d3704504a24eed1375a64bd
Arona Jones <warlordjones@gmail.com>
2015-04-02 22:07:31 +0100

    Metal recipes

--- src/java/fantasykingdoms/common/init/InitRecipes.java
+++ src/java/fantasykingdoms/common/init/InitRecipes.java
@@ -1,11 +1,23 @@
 package fantasykingdoms.common.init;
 
+import net.minecraft.item.ItemStack;
+
+import cpw.mods.fml.common.registry.GameRegistry;
+
+import boilerplate.common.utils.recipe.RecipeUtils;
 
 public class InitRecipes
 {
 	public static void init()
 	{
-
+		for (int meta = 0; meta < 6; meta++)
+		{
+			RecipeUtils.addMetalRecipes(InitBlocks.blockMetal, InitItems.itemIngot, InitItems.itemNugget, meta);
+		}
+		for (int meta = 0; meta < 5; meta++)
+		{
+			GameRegistry.addSmelting(new ItemStack(InitBlocks.blockMetalOre, 1, meta), new ItemStack(InitItems.itemIngot, 1, meta), 0.3F);
+		}
 	}
 
 }
